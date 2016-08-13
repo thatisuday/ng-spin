d1eca7831dc596e7353f0cd1ad20086edce75402
Uday Hiwarale <uhiwarale@gmail.com>
2016-08-13 10:29:28 +0530

    requestError bug fixed

--- src/js/interceptor.js
+++ src/js/interceptor.js
@@ -4,17 +4,25 @@ angular
 .config(['$httpProvider', function($httpProvider){
 	$httpProvider
 	.interceptors
-	.push(['$rootScope', '$timeout', 'ngSpinOps', function($rootScope, $timeout, ngSpinOps){
+	.push(['$rootScope', '$timeout', '$q', 'ngSpinOps', function($rootScope, $timeout, $q, ngSpinOps){
 		return {
 			'request' : function(request){
 				if(request.ngSpin || ngSpinOps.autoGlobal){
 					$timeout(function(){
 						$rootScope.$emit('$ngSpinStart');
 					}, ngSpinOps.delay);
-					
 				}
 
 				return request;
+			},
+			'requestError': function(request) {
+				if(request.ngSpin || ngSpinOps.autoGlobal){
+					$timeout(function(){
+						$rootScope.$emit('$ngSpinStop');
+					}, ngSpinOps.extend);
+				}
+
+				return $q.reject(request);
 			},
 			'response' : function(response){
 				if(response.config.ngSpin || ngSpinOps.autoGlobal){
@@ -32,7 +40,7 @@ angular
 					}, ngSpinOps.extend);
 				}
 
-				return response;
+				return $q.reject(response);
 			}
 		}
 	}]);
